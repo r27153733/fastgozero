6507063cac0ea5f2fc34cf8d8a562e000d3aebac
r27153733 <r27153733@gmail.com>
2024-12-20 10:24:15 +0800

    fix: params_test.go

--- rest/pathvar/params_test.go
+++ rest/pathvar/params_test.go
@@ -1,11 +1,11 @@
 package pathvar
 
 import (
-	"net/http"
 	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/valyala/fasthttp"
 )
 
 func TestVars(t *testing.T) {
@@ -13,16 +13,16 @@ func TestVars(t *testing.T) {
 		"a": "1",
 		"b": "2",
 	}
-	r, err := http.NewRequest(http.MethodGet, "/", nil)
-	assert.Nil(t, err)
-	r = WithVars(r, expect)
-	assert.EqualValues(t, expect, Vars(r))
+	r := fasthttp.RequestCtx{}
+	free := SetVars(&r, expect)
+	assert.EqualValues(t, expect, Vars(&r))
+	free()
+	assert.Nil(t, Vars(&r))
 }
 
 func TestVarsNil(t *testing.T) {
-	r, err := http.NewRequest(http.MethodGet, "/", nil)
-	assert.Nil(t, err)
-	assert.Nil(t, Vars(r))
+	r := fasthttp.RequestCtx{}
+	assert.Nil(t, Vars(&r))
 }
 
 func TestContextKey(t *testing.T) {
